6ba1b5196aa6cc1b0377f68fd082a92470d09960
Rayan Moarkech <rayanzone@hotmail.com>
2022-01-16 15:33:47 -0500

    Added ionic and react

---
+++ covid-19-tracker/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'COVID-19 Tracker',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
