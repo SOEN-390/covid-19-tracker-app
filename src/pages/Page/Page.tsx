52418059b79749fac30d55d92d09324a1bdbc2f2
Rayan Moarkech <rayanzone@hotmail.com>
2022-01-28 11:46:51 -0500

    Moved Page to Page folder

--- src/pages/Page/Page.tsx
+++ src/pages/Page/Page.tsx
@@ -1,6 +1,6 @@
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { useParams } from 'react-router';
-import ExploreContainer from '../components/ExploreContainer';
+import ExploreContainer from '../../components/ExploreContainer';
 import './Page.css';
 
 const Page: React.FC = () => {
