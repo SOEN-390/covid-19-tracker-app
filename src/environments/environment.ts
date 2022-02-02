713fdc249817fcfcefc8d2a55a7b6092b02fd197
Rayan Moarkech <rayanzone@hotmail.com>
2022-02-02 18:10:10 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     environment: 'local',
     apiUrl: 'http://127.0.0.1:8080',
-    apiPrefix: '',
+    apiPrefix: '/api/v1',
     firebaseConfig: {
         apiKey: "AIzaSyCc5OVW6IMigjStRF4UImwwYwgadQ9Up_o",
         authDomain: "cvoid-19-app.firebaseapp.com",
@@ -11,6 +11,5 @@ export const environment = {
         messagingSenderId: "462027331949",
         appId: "1:462027331949:web:a9e9c7517a1bf40fb8a264",
         measurementId: "G-SR3XXFC56M"
-
     }
-}
+};
