f798b593e3f7a25c0cc75ae9c8b70e7b306f531a
Rayan Moarkech <rayanzone@hotmail.com>
2022-02-03 09:27:21 -0500

    Added firebase mock

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,23 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+jest.mock('firebase/compat/app', () => {
+    return {
+        initializeApp: jest.fn(() => true),
+        auth: jest.fn(() => {
+            return {
+                onAuthStateChanged: jest.fn((callback) => callback(null)),
+                createUserWithEmailAndPassword: jest.fn(() => Promise.resolve(true)), // TODO: Send the correct response format
+                signInWithEmailAndPassword: jest.fn(() => Promise.resolve(true)), // TODO: Send the correct response format
+                signOut: jest.fn()
+            }
+        })
+    }
+});
+
 test('renders without crashing', () => {
-  const { baseElement } = render(<App />);
-  expect(baseElement).toBeDefined();
+
+    const {baseElement} = render(<App/>);
+    expect(baseElement).toBeDefined();
+
 });
