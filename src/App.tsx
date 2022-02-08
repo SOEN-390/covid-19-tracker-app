ae516f58e01f4b305a2c2497de38fef063b55e79
alireza ziarizi <alireza.ziaeework@gmail.com>
2022-02-07 19:25:29 -0500

    immigration dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ import '@ionic/react/css/display.css';
 import './theme/variables.css';
 import AppMenu from "./AppMenu";
 import RegisterNext from "./pages/Register/RegisterNext";
+
 import { Pages } from './providers/pages.enum';
 
 setupIonicReact();
