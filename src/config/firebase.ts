a38a00a5a3ebc3448702823db161ba00736fc1dc
Rayan Moarkech <rayanzone@hotmail.com>
2022-02-09 21:09:42 -0500

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,7 +1,7 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-import { environment } from './src/environments/environment';
+import { environment } from '../environments/environment';
 
 const app = firebase.initializeApp(environment.firebaseConfig);
 
