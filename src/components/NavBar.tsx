01aa14a5b0866c1ae85bc300bc6fd2b74de13ddb
alireza ziarizi <alireza.ziaeework@gmail.com>
2022-02-10 19:39:41 -0500

    update navbar components for retrieving

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,27 @@
-import { IonAvatar, IonCol, IonGrid, IonMenuButton, IonRow, IonSearchbar, IonToolbar } from '@ionic/react';
+import { IonAvatar, IonButton, IonCol, IonGrid, IonMenuButton, IonRow, IonSearchbar, IonToolbar } from '@ionic/react';
 import './NavBar.css';
 import logo from '../resources/icon.png';
+import { useState } from 'react';
+
+
 
 function NavBar() {
+   const [searchText, setSearchText] = useState('');
+    console.log(searchText)
     return (
         <IonToolbar>
             <IonMenuButton slot="start" />
             <IonGrid>
                 <IonRow className="ion-align-items-end">
                     <IonCol size="9" size-sd>
-                        <IonSearchbar />
+                    
+                    <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)} 
+                    showCancelButton="never" ></IonSearchbar>  
+                    <IonButton > search </IonButton>
+                                     
+                        
                     </IonCol>
-
+               
                     <IonCol size="1">
                         <IonAvatar className="avatar">
                             <img src={logo} alt=""/>
