f102cc856624ac8ac0716411794042e7f5d082d8
Rayan Moarkech <rayanzone@hotmail.com>
2022-02-11 18:40:27 -0500

    Create ImmigrationOfficer.class.ts

---
+++ src/objects/ImmigrationOfficer.class.ts
@@ -0,0 +1,18 @@
+import { User } from './User.class';
+import { UserType } from '../enum/UserType.enum';
+
+export class ImmigrationOfficer extends User {
+
+    // Constructor
+
+    public constructor (id: string, firstName: string, lastName: string, phoneNumber: string, address: string, email: string) {
+        super(id, firstName, lastName, phoneNumber, address, email);
+    }
+
+    // Methods
+
+    public getRole(): UserType {
+        return UserType.IMMIGRATION_OFFICER;
+    }
+
+}
