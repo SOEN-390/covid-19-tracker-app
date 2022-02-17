1f7ae751c75bb8ffd02c0b0d1496d787f9a7efd0
Rayan Moarkech <rayanzone@hotmail.com>
2022-02-17 16:23:22 -0500

    Pass info of patients to table component

--- src/pages/HealthOfficials/HealthOfficialPatientsPage.tsx
+++ src/pages/HealthOfficials/HealthOfficialPatientsPage.tsx
@@ -1,19 +1,72 @@
-import { IonPage, IonToolbar } from '@ionic/react';
+import { IonButton, IonCol, IonContent, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 
 import NavBar from '../../components/NavBar';
 import HealthOfficialTable from '../../components/HealthOfficialTable';
+import * as React from 'react';
+import { IPatientTableRow } from '../../interfaces/IPatientTableRow';
 
 const HealthOfficialPatientsPage: React.FC = () => {
 
-    // const { name } = useParams<{ name: string; }>(); //to get page name
+    function createData(
+        name: string,
+        status: string,
+        last_update: string,
+        doctor: string,
+        action: string,
+        priority: string,
+        monitor_symptoms: string
+    ): IPatientTableRow {
+        return {name, status, last_update, action, doctor, priority, monitor_symptoms};
+    }
+
+    const rows = [
+        createData('Sarah SalibSarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', ''),
+        createData('Sarah Salib', 'positive', '20-11-2021', 'Kero', 'contact', '1', '')
+    ];
 
     return (
         <IonPage>
 
             <IonToolbar>
-                <NavBar />
+                <NavBar/>
             </IonToolbar>
-            <HealthOfficialTable/>
+            <IonContent>
+                <IonTitle id="title"> Patients </IonTitle>
+                <div>
+                    <IonRow>
+                        <IonCol/>
+                        // These buttons will change the request and rows!
+                        <IonCol class="confirmButton"> <IonButton id="con"
+                                                                  color="favorite">Confirmed</IonButton></IonCol>
+                        <IonCol class="unconfirmedButton"><IonButton color="favorite1">Unconfirmed</IonButton></IonCol>
+                        <IonCol/>
+                    </IonRow>
+                </div>
+                <HealthOfficialTable patientTableRows={rows}/>
+            </IonContent>
 
         </IonPage>
     );
