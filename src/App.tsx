3c49dc96d90e51ba1b1e165f143095a95a662148
Rayan Moarkech <rayanzone@hotmail.com>
2022-02-17 19:49:03 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,22 @@ import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+import PatientRouting from './routings/Patient.routing';
+import RegisterNext from './pages/Register/RegisterNext';
+import {
+    AdminPages,
+    DoctorPages,
+    HealthOfficialPages,
+    ImmigrationOfficerPages,
+    Pages,
+    PatientPages
+} from './providers/pages.enum';
+import { AuthProvider } from './providers/auth.provider';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import DoctorRouting from './routings/Doctor.routing';
+import ImmigrationOfficerRouting from './routings/ImmigrationOfficer.routing';
+import HealthOfficialRouting from './routings/HealthOfficial.routing';
+import AdminRouting from './routings/Admin.routing';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -22,22 +38,6 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import AppMenu from './AppMenu';
-import RegisterNext from './pages/Register/RegisterNext';
-import {
-    AdminPages,
-    DoctorPages,
-    HealthOfficialPages,
-    ImmigrationOfficerPages,
-    Pages,
-    PatientPages
-} from './providers/pages.enum';
-
-import { AuthProvider } from './providers/auth.provider';
-import PrivateRoute from './components/PrivateRoute/PrivateRoute';
-import DoctorRouting from './routings/Doctor.routing';
-import ImmigrationOfficerRouting from './routings/ImmigrationOfficer.routing';
-import HealthOfficialRouting from './routings/HealthOfficial.routing';
 
 setupIonicReact();
 
@@ -59,11 +59,11 @@ const App: React.FC = () => {
                         <Route path="/register/2" exact={true}>
                             <RegisterNext/>
                         </Route>
-                        <PrivateRoute path={PatientPages.home} component={AppMenu}/>
+                        <PrivateRoute path={PatientPages.home} component={PatientRouting}/>
                         <PrivateRoute path={DoctorPages.home} component={DoctorRouting}/>
                         <PrivateRoute path={ImmigrationOfficerPages.home} component={ImmigrationOfficerRouting}/>
                         <PrivateRoute path={HealthOfficialPages.home} component={HealthOfficialRouting}/>
-                        <PrivateRoute path={AdminPages.home} component={AppMenu}/>
+                        <PrivateRoute path={AdminPages.home} component={AdminRouting}/>
                     </IonRouterOutlet>
                 </IonReactRouter>
             </AuthProvider>
