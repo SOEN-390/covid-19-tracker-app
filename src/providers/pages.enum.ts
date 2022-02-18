c867226eee5c6dae5bd6022446d5e4753cf46087
Rayan Moarkech <rayanzone@hotmail.com>
2022-02-17 19:43:17 -0500

    Created enum pages for each roles

--- src/providers/pages.enum.ts
+++ src/providers/pages.enum.ts
@@ -1,11 +1,34 @@
 export enum Pages {
     login = '/login',
-    register = '/register',
+    register = '/register'
+}
+
+export enum PatientPages {
     home = '/home',
     overview = '/home/overview',
     appointments = '/home/appointments',
     symptoms = '/home/symptoms',
-    patientProfile = '/home/patient-profile',
-    immigrationDashboard = '/home/immigration-dashboard',
-    healthOfficialPage ='/home/healthOfficials'
+    patientProfile = '/home/patient-profile'
+}
+
+export enum DoctorPages {
+    home = '/doctor',
+    patientProfile = '/doctor/patient-profile'
+}
+
+export enum HealthOfficialPages {
+    home = '/health-official',
+    patientsPage ='/health-official/patients',
+    patientProfile = '/health-official/patient-profile'
+}
+
+export enum ImmigrationOfficerPages {
+    home = '/immigration-officer',
+    immigrationDashboard = '/immigration-officer/dashboard',
+    patientProfile = '/immigration-officer/patient-profile'
+}
+
+export enum AdminPages {
+    home = '/admin',
+    patientProfile = '/admin/patient-profile'
 }
