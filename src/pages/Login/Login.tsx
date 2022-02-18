6cf8a04621f0be2f5a175e0c6cb58b2fa3cfa68e
Rayan Moarkech <rayanzone@hotmail.com>
2022-02-17 22:43:07 -0500

    Updated login responsiveness

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,14 +1,13 @@
 import {
     IonApp,
-    IonButton,
-    IonCheckbox,
-    IonContent,
-    IonHeader,
+    IonButton, IonCard,
+    IonCheckbox, IonCol,
+    IonContent, IonGrid,
     IonImg,
     IonInput,
     IonItem,
     IonLabel,
-    IonRouterLink,
+    IonRouterLink, IonRow,
     setupIonicReact,
     useIonToast
 } from '@ionic/react';
@@ -37,43 +36,36 @@ const Login: React.FC = () => {
 
     return (
         <IonApp>
-            <IonHeader>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            </IonHeader>
             <IonContent>
-                <IonImg className="login-logo" src={CovidTrackerTransparent}/>
-                <div className="sign-in-center">
-                    <h2 className="login-text">Sign In</h2>
+                <IonImg className="login__logo" src={CovidTrackerTransparent}/>
+                <div className="login__sign-in-title">
+                    <h2 className="login__text">Sign In</h2>
                 </div>
-                <div className="ion-align-items-center; login-form-center">
-                    <IonLabel className="login-text">Email</IonLabel>
-                    <IonInput className="login-text-field" placeholder="Enter your Email" type="text"
+                <div className="ion-align-items-center; login__form">
+                    <IonLabel className="login__text">Email</IonLabel>
+                    <IonInput className="login__text-field" placeholder="Enter your Email" type="text"
                               onIonChange={(e: any) => setEmail(e.target.value)}/>
 
                     <br/><br/>
-                    <IonLabel className="login-text">Password</IonLabel>
-                    <IonInput className="login-text-field" placeholder="Enter your password" type="password"
+                    <IonLabel className="login__text">Password</IonLabel>
+                    <IonInput className="login__text-field" placeholder="Enter your password" type="password"
                               onIonChange={(e: any) => setPassword(e.target.value)}/>
 
-                    <IonItem className="ion-item" lines="none">
+                    <IonItem className="login__ion-item" lines="none">
                         <div slot="start">
                             <IonCheckbox slot="end" className={'custom-checkbox'} checked={checked}/>
                         </div>
                         <IonLabel>Remember me</IonLabel>
-                    </IonItem>
-                    <IonItem className="ion-item" lines="none">
                         <IonRouterLink href="#" color="#4D4D4D" className="underline">Forgot Password?</IonRouterLink>
                     </IonItem>
                     <IonButton onClick={loginUser} size="large" expand="block" fill="solid"
                                color={'dark-blue'}>LOGIN</IonButton>
                     <br/>
-                    <p className={'register-text'}> Do not have an account? <br/>
-                        <a href={Pages.register} className={'register-text-color'}>Register</a>
+                    <p className={'login__register-text'}> Do not have an account? <br/>
+                        <a href={Pages.register} className={'.login__register-btn'}>Register</a>
                     </p>
                 </div>
-
             </IonContent>
-
         </IonApp>
     );
 };
