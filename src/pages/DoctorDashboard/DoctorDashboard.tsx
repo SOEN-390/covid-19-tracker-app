3e8004b61dd28ab61e084f872f6a69b4d2787fa6
Rayan Moarkech <rayanzone@hotmail.com>
2022-02-17 20:53:50 -0500

    Added 2 PieCharts in doctor dashboard

--- src/pages/DoctorDashboard/DoctorDashboard.tsx
+++ src/pages/DoctorDashboard/DoctorDashboard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { IonContent, IonPage } from '@ionic/react';
+import { IonContent, IonPage, IonTitle } from '@ionic/react';
 import NavBar from '../../components/NavBar';
+import { PieChart } from 'react-minimal-pie-chart';
 
 const DoctorDashboard: React.FC = () => {
 
@@ -9,7 +10,22 @@ const DoctorDashboard: React.FC = () => {
             <IonContent>
                 <NavBar />
 
+                <IonTitle>Diagnostics</IonTitle>
+                <PieChart
+                    data={[
+                        { title: 'Positive', value: 10, color: '#E38627' },
+                        { title: 'Negative', value: 15, color: '#C13C37' },
+                        { title: 'Unconfirmed', value: 20, color: '#6A2135' }
+                    ]}
+                />
 
+                <IonTitle>Patients</IonTitle>
+                <PieChart
+                    data={[
+                        { title: 'Men', value: 10, color: '#E38627' },
+                        { title: 'Women', value: 15, color: '#C13C37' }
+                    ]}
+                />;
             </IonContent>
         </IonPage>
     );
