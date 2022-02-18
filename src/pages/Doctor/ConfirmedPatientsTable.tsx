a7121717315c89bc08adf3b151560a596a9ff98b
eordekian94s <68568405+eordekian94s@users.noreply.github.com>
2022-02-18 18:05:00 -0500

    get assinged patients

--- src/pages/Doctor/ConfirmedPatientsTable.tsx
+++ src/pages/Doctor/ConfirmedPatientsTable.tsx
@@ -1,23 +1,25 @@
 import { IonButton, IonCol, IonContent, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import { useState, useEffect } from 'react';
-
 import NavBar from '../../components/NavBar';
-import PatientsPerDoctorTable from '../../components/PatientsTable/PatientsPerDoctorTable';
 import PatientsTable from '../../components/PatientsTable/PatientsTable';
 import { IPatientTableRow } from '../../interfaces/IPatientTableRow';
 import HttpService from '../../providers/http.service';
 import { DoctorPages } from '../../providers/pages.enum';
-import DoctorsPage from '../Admin/Doctors.page';
+import { useAuth } from '../../providers/auth.provider';
 
 const ConfirmedPatientsTable: React.FC = () => {
-    const [patientsArray, setPatientsArray] = useState<IPatientTableRow[]>()
+    const [patientsArray, setPatientsArray] = useState<IPatientTableRow[]>();
+    const {currentProfile } = useAuth();
 
     useEffect(() => {
-        patientsRetrieval();
+        getAssignedPatients();
     }, []);
 
-    async function patientsRetrieval() {
-        HttpService.get(`patients/all`).then(async (response) => {
+    async function getAssignedPatients() {
+        if (!currentProfile) {
+            return;
+        }
+        HttpService.get(`doctors/${currentProfile.id}/patients/assigned`).then(async (response) => {
             console.log('HERE IS THE DATA IN JSON FORM: ', response);
             setPatientsArray(response);
         }).catch((error) => {
