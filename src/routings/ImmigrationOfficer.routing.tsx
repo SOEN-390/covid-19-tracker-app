a1a68df200cc0e72de54e12806cdd94ee5982bf7
Rayan Moarkech <rayanzone@hotmail.com>
2022-02-17 19:43:01 -0500

    Create ImmigrationOfficer.routing.tsx

---
+++ src/routings/ImmigrationOfficer.routing.tsx
@@ -0,0 +1,31 @@
+import { IonRouterOutlet, IonSplitPane, setupIonicReact } from '@ionic/react';
+import { Redirect, Route } from 'react-router-dom';
+import Menu from '../components/Menu';
+import PatientProfile from '../pages/Doctor/PatientProfile';
+import ImmigrationDashboard from '../pages/ImmigrationOfficer/immigrationDashboard';
+
+import { ImmigrationOfficerPages } from '../providers/pages.enum';
+
+setupIonicReact();
+
+const ImmigrationOfficerRouting: React.FC = () => {
+    return (
+        <IonSplitPane contentId="immigration-officer">
+            <Menu/>
+            <IonRouterOutlet id="immigration-officer">
+                <Route path={ImmigrationOfficerPages.home} exact={true}>
+                    <Redirect to={ImmigrationOfficerPages.immigrationDashboard}/>
+                </Route>
+
+                <Route path={ImmigrationOfficerPages.immigrationDashboard}>
+                    <ImmigrationDashboard/>
+                </Route>
+                <Route path={ImmigrationOfficerPages.patientProfile}>
+                    <PatientProfile/>
+                </Route>
+            </IonRouterOutlet>
+        </IonSplitPane>
+    );
+};
+
+export default ImmigrationOfficerRouting;
