770050d6cf421ef52f11fe7be9a311ca71f3f001
eordekian94s <68568405+eordekian94s@users.noreply.github.com>
2022-02-21 11:06:00 -0500

    Update PatientInformation.tsx

--- src/components/PatientInformation/PatientInformation.tsx
+++ src/components/PatientInformation/PatientInformation.tsx
@@ -1,58 +1,70 @@
-import { IonAvatar, IonButton, IonCol, IonContent, IonImg, IonInput, IonItem, IonLabel, IonRow, IonText } from '@ionic/react';
+import { IonAvatar, IonButton, IonCol, IonContent, IonImg, IonInput, IonLabel, IonRow, IonText } from '@ionic/react';
 import './PatientInformation.css';
 import logo from '../../resources/icon.png'
 import HttpService from '../../providers/http.service';
 import { useState } from 'react';
 
 
-
 const PatientInformation: React.FC = () => {
-    const [firstName, setfirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [address, setAddress] = useState('')
     const [medicalNumber, setMedicalNumber] = useState('')
-    const [phoneNumber, setPhoneNumber] = useState('')
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [testResult, setTestResult] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [address, setAddress] = useState('');
+    const [email, setEmail] = useState('');
+    const [dob, setDOB] = useState('');
+    const [gender, setGender] = useState('');
 
 
-    HttpService.get(`patients/${55}`).then(async (response) => {
-        const data = await response.json();
-        setfirstName(data.firstName)
-        setLastName(data.lastName)
-
+    HttpService.get(`patients/${55}`).then(async (data) => {
+        setData(data);
     }).catch((error) => {
+        console.log(error);
     });
 
+    function setData(data: any) {
+        setMedicalNumber(data.medicalId);
+        setFirstName(data.firstName);
+        setLastName(data.lastName);
+        setTestResult(data.testResult);
+        setPhoneNumber(data.phoneNumber);
+        setAddress(data.address);
+        setEmail(data.email);
+        setDOB(data.dob);
+        setGender(data.gender);
+    }
 
 
     return (
         <IonContent>
-            <div id='Container'>
+            <div id="Container">
 
                 <IonRow>
-                    <IonCol size='2'>
+                    <IonCol size="2">
                         <IonAvatar>
-                            <IonImg src={logo} />
+                            <IonImg src={logo}/>
                         </IonAvatar></IonCol>
                     <IonCol>
                         <IonRow>
                             <div>
 
                                 <IonText><strong>First Name</strong></IonText>
-                                <p className='box'> {firstName}  </p>
+                                <p className="box"> {firstName}  </p>
                             </div>
                         </IonRow>
                         <IonRow>
                             <div>
 
                                 <IonText><strong>Medicare Number</strong></IonText>
-                                <p className='box'>EVAN 2022 3900</p>
+                                <p className="box">{medicalNumber}</p>
                             </div>
                         </IonRow>
                         <IonRow>
                             <div>
 
                                 <IonText> <strong>Email</strong></IonText>
-                                <p className='box'>Chirs_evans@gmail.com</p>
+                                <p className="box">{email}</p>
                             </div>
                         </IonRow>
 
@@ -62,14 +74,22 @@ const PatientInformation: React.FC = () => {
                             <div>
 
                                 <IonText><strong>Last Name</strong></IonText>
-                                <p className='box'>{lastName}</p>
+                                <p className="box">{lastName}</p>
                             </div>
                         </IonRow>
                         <IonRow>
                             <div>
 
                                 <IonText><strong>Phone Number</strong></IonText>
-                                <p className='box'>(514)-911-9110</p>
+                                <p className="box">{phoneNumber}</p>
+                            </div>
+                        </IonRow>
+
+                        <IonRow>
+                            <div>
+
+                                <IonText><strong>Date of birth</strong></IonText>
+                                <p className="box">{dob}</p>
                             </div>
                         </IonRow>
 
@@ -80,86 +100,95 @@ const PatientInformation: React.FC = () => {
                             <div>
 
                                 <IonText><strong>Address</strong></IonText>
-                                <p className='box'>123 rue street st tariq</p>
+                                <p className="box">{address}</p>
 
                             </div>
                         </IonRow>
+
                         <IonRow>
                             <div>
 
-                                <IonText><strong>Postal Code</strong></IonText>
-                                <p className='box'>H9j 2v6</p>
+                                <IonText><strong>Test Result</strong></IonText>
+                                <p className="box">{testResult}</p>
+
                             </div>
                         </IonRow>
 
+                        <IonRow>
+                            <div>
+
+                                <IonText><strong>Gender</strong></IonText>
+                                <p className="box">{gender}</p>
+
+                            </div>
+                        </IonRow>
 
                     </IonCol>
 
 
                 </IonRow>
                 <IonRow>
-                    <div className='button'>
-                        <IonCol> <IonButton className='buttonc'>Symptoms form</IonButton> </IonCol>
-                        <IonCol> <IonButton className='buttonc'>Set an Appoiment</IonButton> </IonCol>
-                        <IonCol> <IonButton className='buttonc'>Send Email</IonButton> </IonCol>
+                    <div className="button">
+                        <IonCol> <IonButton className="buttonc">Symptoms form</IonButton> </IonCol>
+                        <IonCol> <IonButton className="buttonc">Set an Appointment</IonButton> </IonCol>
+                        <IonCol> <IonButton className="buttonc">Send Email</IonButton> </IonCol>
                     </div>
                 </IonRow>
                 <IonRow>
                     <table className="blueTable">
                         <thead>
-                            <tr>
-                                <th>Date</th>
-                                <th>Temperature</th>
-                                <th>Breathing</th>
-                                <th>Other Symptomes</th>
-                            </tr>
+                        <tr>
+                            <th>Date</th>
+                            <th>Temperature</th>
+                            <th>Breathing</th>
+                            <th>Other Symptoms</th>
+                        </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>cell1_1</td>
-                                <td>cell2_1</td>
-                                <td>cell3_1</td>
-                                <td>cell4_1</td>
-                            </tr>
-                            <tr>
-                                <td>cell1_2</td>
-                                <td>cell2_2</td>
-                                <td>cell3_2</td>
-                                <td>cell4_2</td>
-                            </tr>
-                            <tr>
-                                <td>cell1_3</td>
-                                <td>cell2_3</td>
-                                <td>cell3_3</td>
-                                <td>cell4_3</td>
-                            </tr>
+                        <tr>
+                            <td>cell1_1</td>
+                            <td>cell2_1</td>
+                            <td>cell3_1</td>
+                            <td>cell4_1</td>
+                        </tr>
+                        <tr>
+                            <td>cell1_2</td>
+                            <td>cell2_2</td>
+                            <td>cell3_2</td>
+                            <td>cell4_2</td>
+                        </tr>
+                        <tr>
+                            <td>cell1_3</td>
+                            <td>cell2_3</td>
+                            <td>cell3_3</td>
+                            <td>cell4_3</td>
+                        </tr>
                         </tbody>
                     </table>
                 </IonRow>
                 <IonRow>
-                    <div className='button'>
-                        <IonCol> <IonButton className='buttonc'>Add Symptomes</IonButton> </IonCol>
-                        <IonCol> <IonButton className='buttonc'>Modify Symptomes</IonButton> </IonCol>
-                        <IonCol> <IonButton className='buttonc'>Delete Symptomes</IonButton> </IonCol>
+                    <div className="button">
+                        <IonCol> <IonButton className="buttonc">Add Symptoms</IonButton> </IonCol>
+                        <IonCol> <IonButton className="buttonc">Modify Symptoms</IonButton> </IonCol>
+                        <IonCol> <IonButton className="buttonc">Delete Symptoms</IonButton> </IonCol>
                     </div>
                 </IonRow>
                 <IonRow>
-                    <div id='Container2'>
+                    <div id="Container2">
                         <IonRow>
-                            <IonCol size='3'><IonLabel>Subject</IonLabel></IonCol>
-                            <IonCol ><IonInput className='login-text-field'></IonInput></IonCol>
+                            <IonCol size="3"><IonLabel>Subject</IonLabel></IonCol>
+                            <IonCol><IonInput className="login-text-field"></IonInput></IonCol>
 
                         </IonRow>
                         <IonRow>
-                            <IonCol size='3'><IonLabel>Description</IonLabel></IonCol>
-                            <IonCol><IonInput className='login-text-field'></IonInput></IonCol>
-                            <IonButton className='buttonc'>Add</IonButton>
+                            <IonCol size="3"><IonLabel>Description</IonLabel></IonCol>
+                            <IonCol><IonInput className="login-text-field"></IonInput></IonCol>
+                            <IonButton className="buttonc">Add</IonButton>
 
                         </IonRow>
 
                     </div>
                 </IonRow>
-
 
 
             </div>
