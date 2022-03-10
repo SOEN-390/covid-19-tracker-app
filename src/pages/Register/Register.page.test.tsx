1e3f0e178d42e87f15f00ea72a1394cd7f85b4a2
Rayan Moarkech <rayanzone@hotmail.com>
2022-03-10 12:44:57 -0500

    Added signup mock

--- src/pages/Register/Register.page.test.tsx
+++ src/pages/Register/Register.page.test.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import RegisterPage from './Register.page';
 
+jest.mock('../../providers/auth.provider', () => ({
+	useAuth: () => ({
+		signup: jest.fn()
+	}),
+}));
+
+
 test('RegisterPage: Renders without crashing', () => {
 	const {baseElement} = render(<RegisterPage />);
 	expect(baseElement).toBeDefined();
