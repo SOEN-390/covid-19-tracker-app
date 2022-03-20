85a1bdabff16dc3d5a1343de2ff8fee137c1a60b
solibe1 <55918425+solibe1@users.noreply.github.com>
2022-03-20 01:21:48 -0400

    Update Patient.class.ts

--- src/objects/Patient.class.ts
+++ src/objects/Patient.class.ts
@@ -14,20 +14,20 @@ export class Patient extends User {
 	private _gender!: Gender;
 	private _lastUpdatedAt?: string;
 	private _flagged?: boolean;
+	private _reminded?: boolean;
 	private _reviewed?: boolean;
 	private _doctorName?: string;
 
 	// Constructor
 
 	public constructor(id: string, firstName: string, lastName: string, email: string, phoneNumber: string, address: string,
-					   medicalId: string, testResult: TestResult, dob: string, gender: Gender, flagged?: boolean) {
+		medicalId: string, testResult: TestResult, dob: string, gender: Gender) {
 		super(id, firstName, lastName, phoneNumber, address);
 		this._medicalId = medicalId;
 		this._email = email;
 		this._testResult = testResult;
 		this._dob = dob;
 		this._gender = gender;
-		this._flagged = flagged;
 	}
 
 	// Getters & Setters
@@ -81,6 +81,14 @@ export class Patient extends User {
 
 	set flagged(value: boolean) {
 		this._flagged = value;
+	}
+
+	get reminded(): boolean {
+		return this._reminded || false;
+	}
+
+	set reminded(value: boolean) {
+		this._reminded = value;
 	}
 
 	get reviewed(): boolean {
