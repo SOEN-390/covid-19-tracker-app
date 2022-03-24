f5afbd1553ea7386f2c429689fa9ff1824bc41d7
Rayan Moarkech <rayanzone@hotmail.com>
2022-03-24 03:15:38 -0400

    Update git_update.sh

--- build/git_update.sh
+++ build/git_update.sh
@@ -10,13 +10,15 @@ done
 
 if [[ $CURRENT_VERSION == '' ]]
 then
-  CURRENT_VERSION='v0.1.0'
+  CURRENT_VERSION='0.1.0'
 fi
-echo "Current Version: $CURRENT_VERSION"
+echo "Current Package.json Version: $CURRENT_VERSION"
 
-# get highest tag number, and add v0.1.0 if doesn't exist
+# get highest tag number in github
 git fetch --prune --unshallow 2>/dev/null
 GITHUB_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
+
+echo "Current GitHub Version: $GITHUB_VERSION"
 
 # replace . with space so can split into an array
 CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
@@ -29,6 +31,7 @@ VNUM2=${CURRENT_VERSION_PARTS[1]}
 GVNUM1=${GITHUB_VERSION_PARTS[0]}
 GVNUM2=${GITHUB_VERSION_PARTS[1]}
 GVNUM3=${GITHUB_VERSION_PARTS[2]}
+echo "Current GitHub Version1: $GVNUM1"
 
 if [[ $VNUM1 == $GVNUM1 && $VNUM2 == $GVNUM2 ]]
 then
@@ -39,7 +42,7 @@ else
 fi
 
 # create new tag
-NEW_TAG="$VNUM1.$VNUM2.$GVNUM3"
+NEW_TAG="v$VNUM1.$VNUM2.$GVNUM3"
 echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
 
 if [[ $NEW_TAG < $GITHUB_VERSION ]]
