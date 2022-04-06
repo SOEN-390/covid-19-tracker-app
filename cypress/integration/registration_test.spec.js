61938fb8d947d3a9bafbe157428736062a938373
YoelSerf <serfatyyoel@gmail.com>
2022-04-06 17:41:44 -0400

    CypressRegistration
    improvement

--- cypress/integration/registration_test.spec.js
+++ cypress/integration/registration_test.spec.js
@@ -1,26 +1,27 @@
 
 describe('Unsuccessful Sign up Process', () => {
-	beforeEach(() => {
-		cy.visit('http://localhost:8100/register');
-	});
 
 	it('Unsuccessful Signup with invalid confirmed password', () => {
-		//input correct email
+
+		cy.visit('http://localhost:8100/register');
+
+		//input email
 		cy.get('[data-cy=email]')
 			.type('emailRegistrationTest@demo.com')
 			.should('have.value', 'emailRegistrationTest@demo.com');
 
-		//input correct password
+		//input password
 		cy.get('[data-cy=password]')
 			.type('Demo123')
 			.should('have.value', 'Demo123');
 
-		//input correct copy of the password
+		//input copy of the password
 		cy.get('[data-cy=confirmed_password]')
 			.type('Demo1234')
 			.should('have.value', 'Demo1234');
 
 		cy.contains('Next').click();
+		cy.url().should('include', '/register');
 	});
 
 });
@@ -29,132 +30,52 @@ describe('Signup Process Success', () => {
 	it('Sign up', () => {
 		cy.visit('http://localhost:8100/register');
 
-		//input correct email
+		//input email
 		cy.get('[data-cy=email]')
 			.type('emailRegistrationTest@demo.com')
 			.should('have.value', 'emailRegistrationTest@demo.com');
 
-		//input correct password
+		//input password
 		cy.get('[data-cy=password]')
-			.type('Demo123')
-			.should('have.value', 'Demo123');
+			.type('emailRegistrationTest')
+			.should('have.value', 'emailRegistrationTest');
 
-		//input correct copy of the password
+		//input copy of the password
 		cy.get('[data-cy=confirmed_password]')
-			.type('Demo123')
-			.should('have.value', 'Demo123');
+			.type('emailRegistrationTest')
+			.should('have.value', 'emailRegistrationTest');
 
 		cy.contains('Next').click();
+		cy.url().should('include', '/register');
 	});
 });
 
-describe('Unsuccessful Personal Information Registration Process', () => {
+describe('Registration Page 2 Process', () => {
 	beforeEach(() => {
 		cy.visit('http://localhost:8100/register/2');
-	});
-
-	it('Unsuccessful Register due to empty First Name', () => {
-		//incorrect input
-		cy.get('[data-cy=first_name]')
-			.type('0')
-			.should('have.value', '0');
-
-		//input correct Last Name
-		cy.get('[data-cy=last_name]')
-			.type('bla')
-			.should('have.value', 'bla');
-
-		// input correct Medical Number
-		cy.get('[data-cy=medical_number]')
-			.type('A12345')
-			.should('have.value', 'A12345');
-
-		//input correct Date of Birth
-		cy.get('[data-cy=date_of_birth]')
-			.type('1990-01-01')
-			.should('have.value', '1990-01-01');
-
-		// input correct address
-		cy.get('[data-cy=address]')
-			.type('Montreal')
-			.should('have.value', 'Montreal');
-
-		// input phone number
-		cy.get('[data-cy=phone_number]')
-			.type('5141234567')
-			.should('have.value', '5141234567');
-
-		// Pick any gender
-		cy.get('[data-cy=gender]')
-
-		// Pick Test Result
-		cy.get('[data-cy=testResult]')
-
-		cy.contains('Register').click();
-	});
-
-	it('Unsuccessful Register due to empty Last Name', () => {
-		//input correct First Name
-		cy.get('[data-cy=first_name]')
-			.type('bla')
-			.should('have.value', 'bla');
-
-		//input incorrect Last Name
-		cy.get('[data-cy=last_name]')
-			.type('0')
-			.should('have.value', '0');
-
-		// input correct Medical Number
-		cy.get('[data-cy=medical_number]')
-			.type('A12345')
-			.should('have.value', 'A12345');
-
-		//input correct Date of Birth
-		cy.get('[data-cy=date_of_birth]')
-			.type('1990-01-01')
-			.should('have.value', '1990-01-01');
-
-		// input correct address
-		cy.get('[data-cy=address]')
-			.type('Montreal')
-			.should('have.value', 'Montreal');
-
-		// input phone number
-		cy.get('[data-cy=phone_number]')
-			.type('5141234567')
-			.should('have.value', '5141234567');
-
-		// Pick any gender
-		cy.get('[data-cy=gender]')
-
-		// Pick Test Result
-		cy.get('[data-cy=testResult]')
-
-		cy.contains('Register').click();
-	});
+	})
 
 	it('Unsuccessful Register due to empty Medical Number', () => {
-			//incorrect input
+			//input
 			cy.get('[data-cy=first_name]')
 				.type('bla')
 				.should('have.value', 'bla');
 
-			//input correct Last Name
+			//input Last Name
 			cy.get('[data-cy=last_name]')
 				.type('bla')
 				.should('have.value', 'bla');
 
-			// input correct Medical Number
+			// input empty Medical Number
 			cy.get('[data-cy=medical_number]')
-				.type('0')
-				.should('have.value', '0');
+				.should('have.value', '');
 
-			//input correct Date of Birth
+			//input Date of Birth
 			cy.get('[data-cy=date_of_birth]')
 				.type('1990-01-01')
 				.should('have.value', '1990-01-01');
 
-			// input correct address
+			// input address
 			cy.get('[data-cy=address]')
 				.type('Montreal')
 				.should('have.value', 'Montreal');
@@ -171,30 +92,31 @@ describe('Unsuccessful Personal Information Registration Process', () => {
 			cy.get('[data-cy=testResult]')
 
 			cy.contains('Register').click();
+			cy.url().should('include', '/2');
 	});
 
 	it('Unsuccessful Register due to invalid Date of birth', () => {
-		//incorrect input
+		//input First Name
 		cy.get('[data-cy=first_name]')
 			.type('bla')
 			.should('have.value', 'bla');
 
-		//input correct Last Name
+		//input Last Name
 		cy.get('[data-cy=last_name]')
 			.type('bla')
 			.should('have.value', 'bla');
 
-		// input correct Medical Number
+		// input Medical Number
 		cy.get('[data-cy=medical_number]')
 			.type('A12345')
 			.should('have.value', 'A12345');
 
-		//input incorrect Date of Birth
+		//input invalid Date of Birth
 		cy.get('[data-cy=date_of_birth]')
 			.type('1000-01-01')
 			.should('have.value', '1000-01-01');
 
-		// input correct address
+		// input address
 		cy.get('[data-cy=address]')
 			.type('Montreal')
 			.should('have.value', 'Montreal');
@@ -211,33 +133,33 @@ describe('Unsuccessful Personal Information Registration Process', () => {
 		cy.get('[data-cy=testResult]')
 
 		cy.contains('Register').click();
+		cy.url().should('include', '/2');
 	});
 
-	it('Unsuccessful Register due to empty Address', () => {
-		//incorrect input
+	it('Unsuccessful Register due to empty Date of birth', () => {
+		//input First Name
 		cy.get('[data-cy=first_name]')
 			.type('bla')
 			.should('have.value', 'bla');
 
-		//input correct Last Name
+		//input Last Name
 		cy.get('[data-cy=last_name]')
 			.type('bla')
 			.should('have.value', 'bla');
 
-		// input correct Medical Number
+		// input Medical Number
 		cy.get('[data-cy=medical_number]')
 			.type('A12345')
 			.should('have.value', 'A12345');
 
-		//input incorrect Date of Birth
+		//input empty Date of Birth
 		cy.get('[data-cy=date_of_birth]')
-			.type('1999-01-01')
-			.should('have.value', '1999-01-01');
+			.should('have.value', '');
 
-		// input correct address
+		// input address
 		cy.get('[data-cy=address]')
-			.type('0')
-			.should('have.value', '0');
+			.type('Montreal')
+			.should('have.value', 'Montreal');
 
 		// input phone number
 		cy.get('[data-cy=phone_number]')
@@ -251,6 +173,48 @@ describe('Unsuccessful Personal Information Registration Process', () => {
 		cy.get('[data-cy=testResult]')
 
 		cy.contains('Register').click();
+		cy.url().should('include', '/2');
+	});
+
+
+	it('Unsuccessful Register due to empty Address', () => {
+		//incorrect input
+		cy.get('[data-cy=first_name]')
+			.type('bla')
+			.should('have.value', 'bla');
+
+		//input Last Name
+		cy.get('[data-cy=last_name]')
+			.type('bla')
+			.should('have.value', 'bla');
+
+		// input Medical Number
+		cy.get('[data-cy=medical_number]')
+			.type('A12345')
+			.should('have.value', 'A12345');
+
+		//input Date of Birth
+		cy.get('[data-cy=date_of_birth]')
+			.type('1999-01-01')
+			.should('have.value', '1999-01-01');
+
+		// input empty address
+		cy.get('[data-cy=address]')
+			.should('have.value', '');
+
+		// input phone number
+		cy.get('[data-cy=phone_number]')
+			.type('5141234567')
+			.should('have.value', '5141234567');
+
+		// Pick any gender
+		cy.get('[data-cy=gender]')
+
+		// Pick Test Result
+		cy.get('[data-cy=testResult]')
+
+		cy.contains('Register').click();
+		cy.url().should('include', '/2');
 	});
 
 	it('Unsuccessful Register due to empty Phone number', () => {
@@ -259,30 +223,29 @@ describe('Unsuccessful Personal Information Registration Process', () => {
 			.type('bla')
 			.should('have.value', 'bla');
 
-		//input correct Last Name
+		//input Last Name
 		cy.get('[data-cy=last_name]')
 			.type('bla')
 			.should('have.value', 'bla');
 
-		// input correct Medical Number
+		// input Medical Number
 		cy.get('[data-cy=medical_number]')
 			.type('A12345')
 			.should('have.value', 'A12345');
 
-		//input incorrect Date of Birth
+		//input Date of Birth
 		cy.get('[data-cy=date_of_birth]')
 			.type('1999-01-01')
 			.should('have.value', '1999-01-01');
 
-		// input correct address
+		// input address
 		cy.get('[data-cy=address]')
 			.type('Montreal')
 			.should('have.value', 'Montreal');
 
-		// input phone number
+		// input empty phone number
 		cy.get('[data-cy=phone_number]')
-			.type('0')
-			.should('have.value', '0');
+			.should('have.value', '');
 
 		// Pick any gender
 		cy.get('[data-cy=gender]')
@@ -291,24 +254,23 @@ describe('Unsuccessful Personal Information Registration Process', () => {
 		cy.get('[data-cy=testResult]')
 
 		cy.contains('Register').click();
+		cy.url().should('include', '/2');
 	});
 });
 
-
-
-describe('Personal Information Registration Process Success', () => {
+describe('Registration Page 2 Success', () => {
 	it('Sign up', () => {
 		cy.visit('http://localhost:8100/register/2');
 
 		//input correct First Name
 		cy.get('[data-cy=first_name]')
-			.type('bla')
-			.should('have.value', 'bla');
+			.type('John')
+			.should('have.value', 'John');
 
 		//input correct Last Name
 		cy.get('[data-cy=last_name]')
-			.type('bla')
-			.should('have.value', 'bla');
+			.type('Smith')
+			.should('have.value', 'Smith');
 
 		// input correct Medical Number
 		cy.get('[data-cy=medical_number]')
