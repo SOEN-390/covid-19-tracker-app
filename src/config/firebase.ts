09c8a0b2f951b0d6d4ef8c05c10da1d15b21ed9e
Rayan Moarkech <rayanzone@hotmail.com>
2022-04-23 16:25:17 -0400

    Fixed issue on phone with firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,8 +2,16 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import environment from '../environments/environment';
+import { Capacitor } from '@capacitor/core';
+import { indexedDBLocalPersistence, initializeAuth } from 'firebase/auth';
 
 const app = firebase.initializeApp(environment.firebaseConfig);
+
+if (Capacitor.isNativePlatform()) {
+	initializeAuth(app, {
+		persistence: indexedDBLocalPersistence
+	});
+}
 
 export const auth = app.auth();
 export default app;
